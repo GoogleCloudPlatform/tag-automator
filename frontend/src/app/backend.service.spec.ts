e7b25fa887419fa9afc56246ee2a295a9f09a876
Ricardo Godoy <rgodoy@google.com>
2024-07-23 18:27:18 -0300

    Adding missing license headers.

--- frontend/src/app/backend.service.spec.ts
+++ frontend/src/app/backend.service.spec.ts
@@ -1,8 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+// Copyright 2024 Google LLC
 
-import { BackendService } from './backend.service';
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 
-describe('BackendService', () => {
+//      https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { TestBed } from "@angular/core/testing";
+
+import { BackendService } from "./backend.service";
+
+describe("BackendService", () => {
   let service: BackendService;
 
   beforeEach(() => {
@@ -10,7 +24,7 @@ describe('BackendService', () => {
     service = TestBed.inject(BackendService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,9 +1,23 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+// Copyright 2024 Google LLC
 
-import { HomeComponent } from './HomeComponent';
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 
-describe('HomeComponent', () => {
+//      https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { waitForAsync, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+
+import { HomeComponent } from "./HomeComponent";
+
+describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
@@ -19,7 +33,7 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should compile', () => {
+  it("should compile", () => {
     expect(component).toBeTruthy();
   });
 });
